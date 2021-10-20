d9f6db0b7e80e797c6dc7efcc06d835232e22538
Layen <Layen@qq.com>
2021-10-20 20:00:55 +0800

    Second commit

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -9,5 +9,6 @@ import java.sql.SQLOutput;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git!");
     }
 }
