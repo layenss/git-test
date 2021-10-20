978d565e9e9cae10389403952d0a116147120203
Layen <Layen@qq.com>
2021-10-20 19:52:51 +0800

    first commit

---
+++ src/main/java/com/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.git;
+
+import java.sql.SQLOutput;
+
+/**
+ * @author shkstart
+ * @create 2021-10-20 19:46
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
